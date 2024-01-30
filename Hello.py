97598f1bb845e5b313cef896d05baa158fcf0d62
Atobi97 <158207670+Atobi97@users.noreply.github.com>
2024-01-30 12:30:38 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,28 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-LOGGER = get_logger(__name__)
+def main():
+    # Set the title of the app
+    st.title("Simple Streamlit App with Graphs")
 
+    # Generate sample data
+    data = pd.DataFrame({
+        'X': np.arange(10),
+        'Y': np.random.randint(1, 10, size=(10,))
+    })
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    # Display the sample data
+    st.write("Sample Data:")
+    st.write(data)
 
-    st.write("# Welcome to Streamlit! 👋")
+    # Create a line chart
+    st.line_chart(data.set_index('X'))
 
-    st.sidebar.success("Select a demo above.")
+    # Create a bar chart
+    st.bar_chart(data.set_index('X'))
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
+# Run the app
 if __name__ == "__main__":
-    run()
+    main()
